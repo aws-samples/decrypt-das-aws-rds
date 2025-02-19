33bb1ac75ae6bab36db35291b47715af18b80733
Vishal <87089581+vishal8387@users.noreply.github.com>
2025-02-19 17:33:31 -0500

    Replaced 'timestamp'to int(time.time() * 1000)

--- rds-das-decrypt-cloudwatch.py
+++ rds-das-decrypt-cloudwatch.py
@@ -5,6 +5,7 @@ import boto3
 import base64
 import zlib
 import uuid
+import time
 import aws_encryption_sdk
 from aws_encryption_sdk import CommitmentPolicy
 from aws_encryption_sdk.internal.crypto import WrappingKey
@@ -109,7 +110,7 @@ def lambda_handler(event, context):
                         logGroupName=LOG_GROUP_NAME,
                         logStreamName=LOG_STREAM_NAME,
                         logEvents=[{
-                            'timestamp': int(context.get_remaining_time_in_millis()),
+                            'timestamp': int(time.time() * 1000),
                             'message': json.dumps(log_event)
                         }]
                     )
